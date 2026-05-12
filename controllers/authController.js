0303bce676facbd3bffb6477ec3743e604c1e28b
johnraeo <johnrae088@gmail.com>
2026-05-13 02:29:12 +0800

    re-comment vars and added truxt proxy to server js

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,10 +57,10 @@ const login = asyncHandler(async (req, res) => {
     // Create secure cookie with refresh token 
     res.cookie('jwt', refreshToken, {
         httpOnly: true, //accessible only by web server 
-        // secure: true, //https
-        // sameSite: 'None', //cross-site cookie 
-        secure: process.env.NODE_ENV === 'production', // only true in prod
-        sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+        secure: true, //https
+        sameSite: 'None', //cross-site cookie 
+        // secure: process.env.NODE_ENV === 'production', // only true in prod
+        // sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match rT
     })
 
